daac32d9fa14a105bdd072741d730f7ac87d2fe9
Pratish Kumar <prkum@microsoft.com>
2018-10-24 15:04:51 +0530

    Init

---
+++ AAAAA.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <utility>
+
+using namespace std;
+
+void solve()
+{
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(--t)
+    {
+        solve();
+    }
+}
---
+++ ABA12C.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+void solve()
+{
+    int n, k;
+    cin>>n>>k;
+
+    vector<int>
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(--t)
+    {
+        solve();
+    }
+}
