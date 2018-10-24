b15dce7de227ac0a003d761b1fe56bd50b80275d
Pratish Kumar <prkum@microsoft.com>
2018-10-24 15:19:00 +0530

    ABA12c_1

--- ABA12C.cpp
+++ ABA12C.cpp
@@ -12,8 +12,24 @@ void solve()
 {
     int n, k;
     cin>>n>>k;
+    map<int, int> kgPrice;
 
-    vector<int>
+    for(int i = 1; i <= k; i++)
+    {
+        int price;
+        cin>>price;
+        kgPrice[i] = price;
+    }
+
+    vector<int> packCount;
+    packCount.reserve()
+    vector<int> minPrice;
+    
+    for(auto it = kgPrice.begin(), it != kgPrice.end(); ++it)
+    {
+    int res = 0;
+
+
 }
 
 int main()
